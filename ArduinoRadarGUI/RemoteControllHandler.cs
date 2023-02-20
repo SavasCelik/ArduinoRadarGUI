0b37e2ce670aececd280613f15db02cbaff47e64
Philipp Schmidd <welaya_luti8@outlook.com>
2023-02-20 10:38:47 +0100

    Create RemoteControllHandler.cs
    Add RemoteControllHandler for handling the commands

---
+++ ArduinoRadarGUI/RemoteControllHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoRadarGUI
+{
+    public class RemoteControllHandler
+    {
+        private readonly Radar _radar;
+
+        public RemoteControllHandler(Radar radar)
+        {
+            _radar = radar;
+        }
+
+        public void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
