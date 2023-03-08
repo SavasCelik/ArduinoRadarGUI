15bc0f2379e100bbd8874a2e6413ba958ab26d57
Savas Celik <scelik.ch@gmail.com>
2023-03-08 18:00:13 +0100

    Rename Window

--- ArduinoRadarGUI/RadarForm.Designer.cs
+++ ArduinoRadarGUI/RadarForm.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "RadarForm";
-            this.Text = "Form1";
+            this.Text = "Arduino Radar";
             this.ResumeLayout(false);
 
         }
